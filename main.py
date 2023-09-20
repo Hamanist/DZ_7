d572b86b91da2890b1bcf860739e0bc15984b8f2
Hamanist <hamanist@gmail.com>
2023-09-20 21:07:56 +0300

    finish main code

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-import os
-
-from function.functions import get_student_by_pk, load_students
+from function.functions import get_student_by_pk, get_profession_by_title, check_fitness
 
 
 def main():
     """
     :return: Основной код программы
+             student -> выводит полное имя и скилы
+             profession -> принимает профессию, после чего проводит анализ на пригодность
+
     """
     user = int(input('Введите номер студента\n'))
     student = get_student_by_pk(user)
 
     if student is None:
         print('У нас нет такого студента')
+        quit()
     else:
         print(f'Студент {student["full_name"]}\nЗнает {", ".join(student["skills"])}')
 
+    user = input(f'Выберите специальность для оценки студента {student["full_name"]}\n').capitalize()
+    profession = get_profession_by_title(user)
 
-    # profession = get_profession_by_title("Frontend")
-    # print(profession)
+    knows_the_skills, no_know_the_skills, suitability = check_fitness(student, profession)
+    if profession is None:
+        print('Нет такой профессии')
+        quit()
+    else:
+        if knows_the_skills == set():
+            knows_the_skills = "абсолютно ни чего по этой профессии"
+        print(f'Пригодность {suitability}%')
+        print(f'{student["full_name"]} знает {", ".join(knows_the_skills)}')
+        print(f'{student["full_name"]} не знает {", ".join(no_know_the_skills)}')
 
-    # user = input('')
-    # statistic = check_fitness(student, profession)
-    # print(statistic)
 
-
-
-
-main()
+if __name__ == '__main__':
+    main()
